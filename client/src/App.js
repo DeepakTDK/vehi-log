041d5d887c7efa4bf1045f1c06653514de0ef27e
Deepak Kumar T <44243734+DeepakTMD@users.noreply.github.com>
2021-12-11 00:09:14 +0530

    client server setup done

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Vehi-Log v1.0</h1>
+    </div>
+  );
+}
+
+export default App;
